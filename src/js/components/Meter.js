0239961f880a009af9d35ad1b5c4acb87bf7167d
Petr Balcar <balcar@dxc.com>
2023-10-10 10:49:03 +0200

    chore(Meter): remove React 17 unsupported LFC method componentWillReceiveProps

--- src/js/components/Meter.js
+++ src/js/components/Meter.js
@@ -31,6 +31,139 @@ function getMaxDecimalDigits (series) {
   return Math.pow(10, maxDigits);
 }
 
+const _normalizeSeries = (props, thresholds) => {
+  let series = [];
+  if (props.series) {
+    series = props.series;
+  } else if (props.value || props.value === 0) {
+    series = [
+      {value: props.value}
+    ];
+    if (props.colorIndex) {
+      series[0].colorIndex = props.colorIndex;
+    }
+  }
+
+  // set color index
+  if (series.length === 1 && props.thresholds) {
+    const item = series[0];
+    if (! item.colorIndex) {
+      // see which threshold color index to use
+      let cumulative = 0;
+      thresholds.some(threshold => {
+        cumulative += threshold.value;
+        if (item.value < cumulative) {
+          item.colorIndex = threshold.colorIndex || 'graph-1';
+          return true;
+        }
+        return false;
+      });
+    }
+  } else {
+    series.forEach((item, index) => {
+      if (! item.colorIndex) {
+        item.colorIndex = `graph-${index + 1}`;
+      }
+    });
+  }
+
+  return series;
+};
+
+const _normalizeThresholds = (props, min, max) => {
+  let thresholds = [];
+  if (props.thresholds) {
+    // Convert thresholds from absolute values to cummulative,
+    // so we can re-use the series drawing code.
+    let priorValue = min;
+    thresholds.push({ hidden: true });
+    for (let i = 0; i < props.thresholds.length; i += 1) {
+      const threshold = props.thresholds[i];
+      // The value for the prior threshold ends at the beginning of this
+      // threshold. Series drawing code expects the end value.
+      thresholds[i].value = threshold.value - priorValue;
+      thresholds.push({
+        colorIndex: threshold.colorIndex
+      });
+      priorValue = threshold.value;
+      if (i === (props.thresholds.length - 1)) {
+        thresholds[thresholds.length-1].value = max - priorValue;
+      }
+    }
+  } else if (props.threshold) {
+    thresholds = [
+      { value: props.threshold, hidden: true },
+      {
+        value: max - props.threshold,
+        colorIndex: 'critical'
+      }
+    ];
+  }
+  return thresholds;
+};
+
+const _seriesTotal = (series) => {
+  const maxDecimalDigits = getMaxDecimalDigits(series);
+  let total = 0;
+  series.forEach((item) => {
+    total += item.value * maxDecimalDigits;
+  });
+
+  return total / maxDecimalDigits;
+};
+
+const _seriesMax = (series) => {
+  let max = 0;
+  series.some(item => {
+    max = Math.max(max, item.value);
+  });
+  return max;
+};
+
+// Generates state based on the provided props.
+const _stateFromProps = (props) => {
+  let total;
+  if (props.series) {
+    total = _seriesTotal(props.series);
+  } else if (props.hasOwnProperty('value')) {
+    total = props.value;
+  } else {
+    total = 0;
+  }
+  let seriesMax;
+  // only care about series max when there are multiple values
+  if (props.series && props.series.length > 1) {
+    seriesMax = _seriesMax(props.series);
+  }
+  // Normalize min and max
+  const min = (props.min || 0);
+  // Max could be provided in props or come from the total of
+  // a multi-value series.
+  const max = (props.max ||
+    (props.stacked ? Math.max(seriesMax, total || 0, 100) :
+      (seriesMax || Math.max(total || 0, 100))));
+  // Normalize simple threshold prop to an array, if needed.
+  const thresholds = _normalizeThresholds(props, min, max);
+  // Normalize simple value prop to a series, if needed.
+  const series = _normalizeSeries(props, thresholds);
+
+  let nextState = {
+    series: series,
+    thresholds: thresholds,
+    min: min,
+    max: max,
+    total: total
+  };
+
+  if (props.hasOwnProperty('activeIndex')) {
+    nextState.activeIndex = props.activeIndex;
+  } else if (props.hasOwnProperty('active')) {
+    nextState.activeIndex = props.active ? 0 : undefined;
+  }
+
+  return nextState;
+};
+
 export default class Meter extends Component {
 
   constructor(props, context) {
@@ -39,10 +172,13 @@ export default class Meter extends Component {
     this._onResponsive = this._onResponsive.bind(this);
     this._initialTimeout = this._initialTimeout.bind(this);
     this._onActivate = this._onActivate.bind(this);
-
-    this.state = this._stateFromProps(props);
+    this.state = {};
     this.state.initial = true;
     this.state.limitMeterSize = false;
+  }
+
+  static getDerivedStateFromProps(props) {
+    return _stateFromProps(props);
   }
 
   componentDidMount () {
@@ -53,150 +189,12 @@ export default class Meter extends Component {
     this._initialTimer = setTimeout(this._initialTimeout, 10);
   }
 
-  componentWillReceiveProps (nextProps) {
-    let state = this._stateFromProps(nextProps);
-    this.setState({ ...state });
-  }
-
   componentWillUnmount () {
     clearTimeout(this._initialTimer);
 
     if (this._responsive) {
       this._responsive.stop();
     }
-  }
-
-  _normalizeSeries (props, thresholds) {
-    let series = [];
-    if (props.series) {
-      series = props.series;
-    } else if (props.value || props.value === 0) {
-      series = [
-        {value: props.value}
-      ];
-      if (props.colorIndex) {
-        series[0].colorIndex = props.colorIndex;
-      }
-    }
-
-    // set color index
-    if (series.length === 1 && props.thresholds) {
-      const item = series[0];
-      if (! item.colorIndex) {
-        // see which threshold color index to use
-        let cumulative = 0;
-        thresholds.some(threshold => {
-          cumulative += threshold.value;
-          if (item.value < cumulative) {
-            item.colorIndex = threshold.colorIndex || 'graph-1';
-            return true;
-          }
-          return false;
-        });
-      }
-    } else {
-      series.forEach((item, index) => {
-        if (! item.colorIndex) {
-          item.colorIndex = `graph-${index + 1}`;
-        }
-      });
-    }
-
-    return series;
-  }
-
-  _normalizeThresholds (props, min, max) {
-    let thresholds = [];
-    if (props.thresholds) {
-      // Convert thresholds from absolute values to cummulative,
-      // so we can re-use the series drawing code.
-      let priorValue = min;
-      thresholds.push({ hidden: true });
-      for (let i = 0; i < props.thresholds.length; i += 1) {
-        const threshold = props.thresholds[i];
-        // The value for the prior threshold ends at the beginning of this
-        // threshold. Series drawing code expects the end value.
-        thresholds[i].value = threshold.value - priorValue;
-        thresholds.push({
-          colorIndex: threshold.colorIndex
-        });
-        priorValue = threshold.value;
-        if (i === (props.thresholds.length - 1)) {
-          thresholds[thresholds.length-1].value = max - priorValue;
-        }
-      }
-    } else if (props.threshold) {
-      thresholds = [
-        { value: props.threshold, hidden: true },
-        {
-          value: max - props.threshold,
-          colorIndex: 'critical'
-        }
-      ];
-    }
-    return thresholds;
-  }
-
-  _seriesTotal (series) {
-    const maxDecimalDigits = getMaxDecimalDigits(series);
-    let total = 0;
-    series.forEach((item) => {
-      total += item.value * maxDecimalDigits;
-    });
-
-    return total / maxDecimalDigits;
-  }
-
-  _seriesMax (series) {
-    let max = 0;
-    series.some(item => {
-      max = Math.max(max, item.value);
-    });
-    return max;
-  }
-
-  // Generates state based on the provided props.
-  _stateFromProps (props) {
-    let total;
-    if (props.series) {
-      total = this._seriesTotal(props.series);
-    } else if (props.hasOwnProperty('value')) {
-      total = props.value;
-    } else {
-      total = 0;
-    }
-    let seriesMax;
-    // only care about series max when there are multiple values
-    if (props.series && props.series.length > 1) {
-      seriesMax = this._seriesMax(props.series);
-    }
-    // Normalize min and max
-    const min = (props.min || 0);
-    // Max could be provided in props or come from the total of
-    // a multi-value series.
-    const max = (props.max ||
-      (props.stacked ? Math.max(seriesMax, total || 0, 100) :
-        (seriesMax || Math.max(total || 0, 100))));
-    // Normalize simple threshold prop to an array, if needed.
-    const thresholds = this._normalizeThresholds(props, min, max);
-    // Normalize simple value prop to a series, if needed.
-    const series = this._normalizeSeries(props, thresholds);
-
-    let nextState = {
-      series: series,
-      thresholds: thresholds,
-      min: min,
-      max: max,
-      total: total
-    };
-
-    if (props.hasOwnProperty('activeIndex')) {
-      nextState.activeIndex = props.activeIndex;
-    } else if (props.hasOwnProperty('active')) {
-      nextState.activeIndex = props.active ? 0 : undefined;
-    }
-
-    return nextState;
   }
 
   _initialTimeout () {
